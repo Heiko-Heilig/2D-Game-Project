8420054bbdd3e965c102da72d06b977d102a2167
Heiko Heilig <heiko@heikoheilig.com>
2024-08-19 12:47:20 -0700

    updated dialogue data

--- src/constants.js
+++ src/constants.js
@@ -1,22 +1,33 @@
 export const scaleFactor = 3;
 
 export const dialogueData = {
-    computer: `This is my PC. I am currently doing the Front-End-Engineering Course on Codecademy and I watch tons of UX Tutorials. Who knows, maybe someday I'll turn into a UX Unicorn!`,
+  computer: `This is my PC. I am currently doing the Front-End Engineering course on Codecademy and watching tons of UX tutorials. Who knows, maybe someday I'll turn into a UX Unicorn!`,
 
-    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-     <a href="https://www.youtube.com/@UICollectiveDesign" target="_blank">UI Collective</a>, <a href="https://www.youtube.com/@InteractionDesignOrg" target="_blank">Interaction Design Foundation</a>,
-    <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp</a> (sometimes) and <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygULcmljayBhc3RsZXk%3D" target="_blank">AstleyUX</a>!`,
-    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-   resume: `This is my resume. Who put it on the wall? anyway, <br> <a href="https://heikoheilig.com/wp-content/uploads/2024/08/Heiko_Polinski_UX_Designer_Resume.pdf" target="_blank">Here you can check it out</a>
-    Contact me at heiko@heikoheilig.com.com if you have any interesting job opportunities!`,
-    masterDiploma: `That's my Masters Degree in Culture, Arts & Media Studies. Pretty proud of that one. For my Master Thesis I interviewed Fiverr users from all over the world to find out more about their work. `,
-    books: `Here are my books. I try to alternate between fiction and non-fiction. When I don't read design or  or mystery UX related books, I'm probably burying my face in a horror novel.`,
-    exit: `If you want to exit my portfolio, just close the tab.`,
-    cs1: `This is my first case study. Here I updated a pretty chaotic UI of a Saas Solution and enhanced the navigability and overall clarity. <br> <a href="https://heikoheilig.com/from-chaos/" target="_blank">Check it out?</a>`,
-    cs2: `This is case study number 2. Our Goal here was to drive a mobile app's engagement levels by improving its visual design. <br> <a href="https://heikoheilig.com/case-study-going-with-the-time/" target="_blank">Have a look!</a>`,
-    cs3: `This is my third case study. By introducing gamification principles to an app, we were able to increase retention and task completion rates. And we worked directly with students, which was tons of fun! <br> <a href="https://heikoheilig.com/case-study-say-hi-to-berry/" target="_blank">Here it is!</a>`,
-    console:`Oh, my old GameCube! I like playing older games. These days I play more on PC though.`,
-    pots:`This is just my trash bin. My portfolio isn't in here, silly!.`,
-    picture:`This is picture my wife and I back in the Philippines. I used to live there for a year before we moved to Canada.`,
-    collectable:`This is my Roronoa Zoro collectable, I'm a nerd, so what? Leave me alone.(...But still hire me, please).`,
-  };
+  tv: `That's my TV. I've been watching a lot of tech YouTubers recently, like 
+  <a href="https://www.youtube.com/@UICollectiveDesign" target="_blank">UI Collective</a>, <a href="https://www.youtube.com/@InteractionDesignOrg" target="_blank">Interaction Design Foundation</a>, 
+  <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp</a> (sometimes), and <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygULcmljayBhc3RsZXk%3D" target="_blank">AstleyUX</a>!`,
+
+  bed: `This is where I sleep. Great ideas come to me when I'm lying on my bed. When an idea strikes, I often have to write it down, or else I won't be able to sleep because my mental energy is consumed by it.`,
+
+  resume: `This is my resume. Who put it on the wall? Anyway, <br> <a href="https://heikoheilig.com/wp-content/uploads/2024/08/Heiko_Polinski_UX_Designer_Resume.pdf" target="_blank">you can check it out here</a>. Contact me at heiko@heikoheilig.com if you have any interesting job opportunities!`,
+
+  masterDiploma: `That's my Master's degree in Culture, Arts & Media Studies. I'm pretty proud of that one. For my Master's thesis, I interviewed Fiverr users from all over the world to learn more about their work.`,
+
+  books: `Here are my books. I try to alternate between fiction and non-fiction. When I'm not reading design or UX-related books, I'm probably burying my face in a horror novel.`,
+
+  exit: `If you want to exit my portfolio, just close the tab.`,
+
+  cs1: `This is my first case study. Here, I updated a chaotic UI of a SaaS solution and enhanced its navigability and overall clarity. <br> <a href="https://heikoheilig.com/from-chaos/" target="_blank">Check it out?</a>`,
+
+  cs2: `This is case study number 2. Our goal here was to drive a mobile app's engagement levels by improving its visual design. <br> <a href="https://heikoheilig.com/case-study-going-with-the-time/" target="_blank">Have a look!</a>`,
+
+  cs3: `This is my third case study. By introducing gamification principles to an app, we were able to increase retention and task completion rates. We worked directly with students, which was tons of fun! <br> <a href="https://heikoheilig.com/case-study-say-hi-to-berry/" target="_blank">Here it is!</a>`,
+
+  console: `Oh, my old GameCube! I like playing older games. These days, I play more on PC though.`,
+
+  pots: `This is just my trash bin. My portfolio isn't in here, silly!`,
+
+  picture: `This is a picture of my wife and me back in the Philippines. I used to live there for a year before we moved to Canada.`,
+
+  collectable: `This is my Roronoa Zoro collectible. I'm a nerd, so what? Leave me alone. (...But still hire me, please).`,
+};
