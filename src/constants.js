f67fb8f60ecfb349b32ea8f4390685c8fb5c71f2
Heiko Heilig <heiko@heikoheilig.com>
2024-08-19 12:35:20 -0700

    final

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 3;
+
+export const dialogueData = {
+    computer: `This is my PC. I am currently doing the Front-End-Engineering Course on Codecademy and I watch tons of UX Tutorials. Who knows, maybe someday I'll turn into a UX Unicorn!`,
+
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@UICollectiveDesign" target="_blank">UI Collective</a>, <a href="https://www.youtube.com/@InteractionDesignOrg" target="_blank">INteraction Design Foundation</a>,
+    <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp</a> (sometimes) and <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygULcmljayBhc3RsZXk%3D" target="_blank">AstleyUX</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+   resume: `This is my resume. Who put it on the wall? anyway, <br> <a href="https://heikoheilig.com/wp-content/uploads/2024/08/Heiko_Polinski_UX_Designer_Resume.pdf" target="_blank">Here you can check it out</a>
+    Contact me at heiko@heikoheilig.com.com if you have any interesting job opportunities!`,
+    masterDiploma: `That's my Masters Degree in Culture, Arts & Media Studies. Pretty proud of that one. For my Master Thesis I interviewed Fiverr users from all over the world to find out more about their work. `,
+    books: `Here are my books. I try to alternate between fiction and non-fiction. When I don't read design or  or mystery UX related books, I'm probably burying my face in a horror novel.`,
+    exit: `If you want to exit my portfolio, just close the tab.`,
+    cs1: `This is my first case study. Here I updated a pretty chaotic UI of a Saas Solution and enhanced the navigability and overall clarity. <br> <a href="https://heikoheilig.com/from-chaos/" target="_blank">Check it out?</a>`,
+    cs2: `This is case study number 2. Our Goal here was to drive a mobile app's engagement levels by improving its visual design. <br> <a href="https://heikoheilig.com/case-study-going-with-the-time/" target="_blank">Have a look!</a>`,
+    cs3: `This is my third case study. By introducing gamification principles to an app, we were able to increase retention and task completion rates. And we worked directly with students, which was tons of fun! <br> <a href="https://heikoheilig.com/case-study-say-hi-to-berry/" target="_blank">Here it is!</a>`,
+    console:`Oh, my old GameCube! I like playing older games. These days I play more on PC though.`,
+    pots:`This is just my trash bin. My portfolio isn't in here, silly!.`,
+    picture:`This is picture my wife and I back in the Philippines. I used to live there for a year before we moved to Canada.`,
+  };
